0e166bf131463a16e3a03ffc876afe7921ca6983
Rajib Paul <53445533+rajib3728@users.noreply.github.com>
2022-11-24 15:42:38 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,402 @@
+function fun1()
+{
+document.getElementById("st4").style.backgroundColor="blueviolet";
+}
+function fun2()
+{
+    document.getElementById("st4").style.backgroundColor="blue";
+}
+function fun3()
+{
+    document.getElementById("st4").style.backgroundColor="cyan";
+}
+function fun4()
+{
+    document.getElementById("st4").style.backgroundColor="rgb(20, 236, 20)";
+}
+function fun5()
+{
+    document.getElementById("st4").style.backgroundColor="yellow";
+}
+function fun6()
+{
+    document.getElementById("st4").style.backgroundColor="orange";
+}
+function fun7()
+{
+    document.getElementById("st4").style.backgroundColor="red";
+}
+function fun8()
+{
+    document.getElementById("st4").style.backgroundColor="rgba(255, 255, 255, 0.882)";
+}
+function hello()
+       {
+        var m=document.getElementsByClassName("st3");
+        var i;
+        for (i = 0; i < m.length; i++) {
+          m[i].style.backgroundColor= "whitesmoke";
+         }
+       }
+window.addEventListener("keydown",(event)=>{
+    if(event.key=='a' || event.key=='A'){
+        document.getElementsByClassName("st3")[0].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='b' || event.key=='B'){
+        document.getElementsByClassName("st3")[1].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='C' || event.key=='c'){
+        document.getElementsByClassName("st3")[2].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='d' || event.key=='D'){
+        document.getElementsByClassName("st3")[3].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='E' || event.key=='e'){
+        document.getElementsByClassName("st3")[4].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='f' || event.key=='F'){
+        document.getElementsByClassName("st3")[5].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='G' || event.key=='g'){
+        document.getElementsByClassName("st3")[6].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='h' || event.key=='H'){
+        document.getElementsByClassName("st3")[7].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='I' || event.key=='i'){
+        document.getElementsByClassName("st3")[8].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='j' || event.key=='J'){
+        document.getElementsByClassName("st3")[9].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='K' || event.key=='k'){
+        document.getElementsByClassName("st3")[10].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='l' || event.key=='L'){
+        document.getElementsByClassName("st3")[11].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='M' || event.key=='m'){
+        document.getElementsByClassName("st3")[12].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='n' || event.key=='N'){
+        document.getElementsByClassName("st3")[13].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='O' || event.key=='o'){
+        document.getElementsByClassName("st3")[14].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='p' || event.key=='P'){
+        document.getElementsByClassName("st3")[15].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='Q' || event.key=='q'){
+        document.getElementsByClassName("st3")[16].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='r' || event.key=='R'){
+        document.getElementsByClassName("st3")[17].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='s' || event.key=='S'){
+        document.getElementsByClassName("st3")[18].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='T' || event.key=='t'){
+        document.getElementsByClassName("st3")[19].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='u' || event.key=='U'){
+        document.getElementsByClassName("st3")[20].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='V' || event.key=='v'){
+        document.getElementsByClassName("st3")[21].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='W' || event.key=='w'){
+        document.getElementsByClassName("st3")[22].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='x' || event.key=='X'){
+        document.getElementsByClassName("st3")[23].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='y' || event.key=='Y'){
+        document.getElementsByClassName("st3")[24].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key=='z' || event.key=='Z'){
+        document.getElementsByClassName("st3")[25].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==0){
+        document.getElementsByClassName("st3")[26].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==1){
+        document.getElementsByClassName("st3")[27].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==2){
+        document.getElementsByClassName("st3")[28].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==3){
+        document.getElementsByClassName("st3")[29].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==4){
+        document.getElementsByClassName("st3")[30].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==5){
+        document.getElementsByClassName("st3")[31].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==6){
+        document.getElementsByClassName("st3")[32].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==7){
+        document.getElementsByClassName("st3")[33].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==8){
+        document.getElementsByClassName("st3")[34].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+    else if(event.key==9){
+        document.getElementsByClassName("st3")[35].style.backgroundColor="orange";
+        setTimeout(hello,200);
+    }
+}
+)
+
+/*logic for replacing specific letter with another letter*/
+
+  function fun9(){
+    
+     var x=document.getElementById("st4").value;
+     var x1=document.getElementById("val1").value;
+     var x2=document.getElementById("val2").value;
+     var x3=document.getElementById("val3").value;
+     var x4=document.getElementById("val4").value;
+     var x5=document.getElementById("val5").value;
+     var x6=document.getElementById("val6").value;
+     var x7=document.getElementById("val7").value;
+     var x8=document.getElementById("val8").value;
+     var x9=document.getElementById("val9").value;
+     var x10=document.getElementById("val10").value;
+     var x11=document.getElementById("val11").value;
+     var x12=document.getElementById("val12").value;
+     var x13=document.getElementById("val13").value;
+     var x14=document.getElementById("val14").value;
+     var x15=document.getElementById("val15").value;
+     var x16=document.getElementById("val16").value;
+     var x17=document.getElementById("val17").value;
+     var x18=document.getElementById("val18").value;
+     var x19=document.getElementById("val19").value;
+     var x20=document.getElementById("val20").value;
+     var x21=document.getElementById("val21").value;
+     var x22=document.getElementById("val22").value;
+     var x23=document.getElementById("val23").value;
+     var x24=document.getElementById("val24").value;
+     var x25=document.getElementById("val25").value;
+     var x26=document.getElementById("val26").value;
+
+    if(x=="")
+    {
+        alert("please provide text to encrypt");
+    }
+    else{
+        alert("please scroll the page for seeing the result");
+        
+         /*
+         previous logic:
+
+         function replaceAll(string, search, replace) {
+            return string.split(search).join(replace);
+          }
+          const op1=replaceAll(x, 'a', x1);
+          const op2=replaceAll(op1, 'b', x2);
+          const op3=replaceAll(op2, 'c', x3);
+          const op4=replaceAll(op3, 'd', x4);
+         const op5=replaceAll(op4, 'e', x5);
+         const op6= replaceAll(op5, 'f', x6);
+         const op7=replaceAll(op6, 'g', x7);
+         const op8= replaceAll(op7, 'h', x8);
+         const op9= replaceAll(op8, 'i', x9);
+         const op10= replaceAll(op9, 'j', x10);
+          const op11=replaceAll(op10, 'k', x11);
+          const op12=replaceAll(op11, 'l', x12);
+         const op13= replaceAll(op12, 'm', x13);
+          const op14=replaceAll(op13, 'n', x14);
+        const op15=replaceAll(op14, 'o', x15);
+         const op16= replaceAll(op15, 'p', x16);
+         const op17= replaceAll(op16, 'q', x17);
+         const op18= replaceAll(op17, 'r', x18);
+        const op19 = replaceAll(op18, 's', x19);
+        const op20=  replaceAll(op19, 't', x20);
+        const op21 = replaceAll(op20, 'u', x21);
+        const op22=  replaceAll(op21, 'v', x22);
+        const op23 = replaceAll(op22, 'w', x23);
+        const op24=  replaceAll(op23, 'x', x24);
+        const op25 = replaceAll(op24, 'y', x25);
+        const op26 = replaceAll(op25, 'z', x26);
+          
+          document.getElementById("val27").innerText=op1;*/
+
+        /*  var up = document.getElementById('GFG_UP');
+          var down = document.getElementById('GFG_DOWN');
+          var str = "I have a Lenovo Laptop, a Honor Phone, and a Samsung Tab.";*/
+          var Obj = {
+              a: x1,
+              b: x2,
+              c: x3,
+              d: x4,
+              e: x5,
+              f: x6,
+              g: x7,
+              h: x8,
+              i: x9,
+              j: x10,
+              k: x11,
+              l: x12,
+              m: x13,
+              n: x14,
+              o: x15,
+              p: x16,
+              q: x17,
+              r: x18,
+              s: x19,
+              t: x20,
+              u: x21,
+              v: x22,
+              w: x23,
+              x: x24,
+              y: x25,
+              z: x26
+          };
+      /*    up.innerHTML = str;*/
+    
+          
+            document.getElementById("val27").innerHTML = x.replace(/a|b|c|d|e|f|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z/gi,  function(matched){
+                  return Obj[matched];
+              });
+          
+
+    }
+    
+  }
+  /* 
+           logic for copy text
+  */
+
+function fun10()
+{
+    var x=document.getElementById("st4").value
+    if(x=="")
+    {
+        alert("please provide text to encrypt");
+    }
+    else{
+        let text = document.getElementById("val27").innerText;
+        navigator.clipboard.writeText(text)
+          .then(() => {
+            alert('Text copied to clipboard');
+          })
+          .catch(err => {
+            alert('Error in copying text: ', err);
+          });
+        
+    }
+
+}
+ function fun11()
+ {
+    var x=document.getElementById("st4").value
+    if(x=="")
+    {
+        alert("text is not given");
+    }
+    else{
+    y=confirm("want to clear?")
+    if(y)
+    {
+        document.getElementById("val27").innerText="";
+    }
+    }
+ }
+  
+ function fun12()
+ {
+    var x1=document.getElementById("val1").value;
+    var x2=document.getElementById("val2").value;
+    var x3=document.getElementById("val3").value;
+    var x4=document.getElementById("val4").value;
+    var x5=document.getElementById("val5").value;
+    var x6=document.getElementById("val6").value;
+    var x7=document.getElementById("val7").value;
+    var x8=document.getElementById("val8").value;
+    var x9=document.getElementById("val9").value;
+    var x10=document.getElementById("val10").value;
+    var x11=document.getElementById("val11").value;
+    var x12=document.getElementById("val12").value;
+    var x13=document.getElementById("val13").value;
+    var x14=document.getElementById("val14").value;
+    var x15=document.getElementById("val15").value;
+    var x16=document.getElementById("val16").value;
+    var x17=document.getElementById("val17").value;
+    var x18=document.getElementById("val18").value;
+    var x19=document.getElementById("val19").value;
+    var x20=document.getElementById("val20").value;
+    var x21=document.getElementById("val21").value;
+    var x22=document.getElementById("val22").value;
+    var x23=document.getElementById("val23").value;
+    var x24=document.getElementById("val24").value;
+    var x25=document.getElementById("val25").value;
+    var x26=document.getElementById("val26").value;
+    var y1="your code is: "+x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11+x12+x13+x14+x15+x16+x17+x18+x19+x20+x21+x22+x23+x24+x25+x26;
+    var y2=x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11+x12+x13+x14+x15+x16+x17+x18+x19+x20+x21+x22+x23+x24+x25+x26;
+    if(y2=="")
+    {
+        alert("replace letter for generating output");
+    }
+    else{
+    document.getElementById("val28").innerHTML=y1;
+    alert("code generated");
+    }
+ }
+ /* 
+ replacing multiple letter in single time
+
+        var up = document.getElementById('GFG_UP');
+        var down = document.getElementById('GFG_DOWN');
+        var str = "I have a Lenovo Laptop, a Honor Phone, and a Samsung Tab.";
+        var Obj = {
+            Lenovo: "Dell",
+            Honor: "OnePlus",
+            Samsung: "Lenovo"
+        };
+        up.innerHTML = str;
+  
+        function GFG_Fun() {
+            down.innerHTML = str.replace(/Lenovo|Honor|Samsung/gi, function(matched){
+                return Obj[matched];
+            });
+        }
+ */ 
